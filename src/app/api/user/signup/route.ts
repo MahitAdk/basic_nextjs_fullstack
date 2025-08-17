9dcc685123bcdc197e3f7d44d367a2d9706563d8
Mahit <mahitraj2004@gmail.com>
2025-08-17 23:40:02 +0545

    Signup backend

--- src/app/api/user/signup/route.ts
+++ src/app/api/user/signup/route.ts
@@ -1,6 +1,8 @@
 import { connectDB } from "@/dbConfig/dbConfig";
 import User from "@/models/usermodel";
 import { NextRequest,NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+import {sendmail} from "@/helper/mailer";
 
 connectDB();
 
@@ -10,8 +12,35 @@ export async function POST(request:NextRequest){
         const reqbody=request.json()
         const {username,email,password}=await reqbody;
 
+        const user=await User.findOne({email})
+
+        if(user){
+            return NextResponse.json({error:'User already exists'},{status: 400});
+        }
+
+         const Salt=await bcryptjs.genSalt(10)//Generates a unique salt for password hashing
+
+    //salt is a unique, pseudorandom string that is added to a password before it is hashed
+
+    const Hashedpassword=await bcryptjs.hash(password,Salt)
+
+    const newUser=new User({
+        username,email,password:Hashedpassword
+    })
+
+    const savedUser=await newUser.save()
+
+    await sendmail({email,emailtype:"VERIFY",userId:savedUser._id})
+
+    //Send verification email logic can be added here
+
+
+
     }catch(error:any){
         return NextResponse.json({error:'Error '},{status: 500});
     }
+
+   
+
 
 }
--- src/helper/mailer.ts
+++ src/helper/mailer.ts
@@ -1,8 +1,18 @@
 import nodemailer from "nodemailer";
+import User from "@/models/usermodel";
+import bcryptjs from "bcryptjs";
 
 export const sendmail=async({email,emailtype,userId}:any)=>{
 
 try{
+
+  const hashedToken=await bcryptjs.hash(userId.toString(),10)
+
+  if(emailtype==="VERIFY"){
+    await User.findByIdAndUpdate(userId,{verifyToken:hashedToken,verifyTokenExpiry:Date.now()+3600000}) // 1 hour expiry
+  }//elseif emailtype==="RESET"{ perform same operation for reset password } as above
+
+  //Replace with your SMTP configuration via mailtrap
   const transporter = nodemailer.createTransport({
    host: "smtp.ethereal.email",
    port: 587,
@@ -22,7 +32,9 @@ try{
     to: email,
     subject: emailtype==='verify'?'Verify your email':'Reset your password',
     text: "verify your email?", // plain‑text body
-    html: "<b>Hello world?</b>", // HTML body
+    html: `<p>Click <a href="${process.env.DOMAIN}/verifyemail?token=${hashedToken}">here</a> to ${emailtype === "VERIFY" ? "verify your email" : "reset your password"}
+            or copy and paste the link below in your browser. <br> ${process.env.DOMAIN}/verifyemail?token=${hashedToken}
+            </p>`
   
    }
 
